0d30a647c2d6aead3f73849839b93b035b47d7bd
Jhonny Oliveira <jhonny.oliveira@gmail.com>
2025-05-16 20:26:59 +0000

    Simplify logic.

--- wp-next-post-navi.php
+++ wp-next-post-navi.php
@@ -256,29 +256,29 @@ if($options['comingsoon'] != "no"){
 			{
 				//check security
 				check_admin_referer('wp-next-post-navi-by-kharim');
-				$settings['nav_within_cat'] = isset($_POST['nav_within_cat'])? "1" : "0" ;
-				$settings['is_active'] = isset($_POST['is_active'])? "1" : "0" ;
-				$settings['is_reversed'] = isset($_POST['is_reversed'])? "1" : "0" ;
-				$settings['position'] = isset($_POST['position'])? $_POST['position'] : "bottom" ;
-				$settings['comingsoon'] = isset($_POST['comingsoon'])? $_POST['comingsoon'] : "yes" ;
-				$settings['is_custom'] = isset($_POST['is_custom'])? $_POST['is_custom'] : "yes" ;
-				$settings['corner'] = isset($_POST['corner'])? $_POST['corner'] : "0" ;
-				$settings['shadow'] = isset($_POST['shadow'])? $_POST['shadow'] : "yes" ;
-				$settings['related'] = isset($_POST['related'])? $_POST['related'] : "yes" ;
-				$settings['style'] = isset($_POST['style_css'])? $_POST['style_css'] : "" ;
-				$settings['is_button'] = isset($_POST['is_button'])? "1" : "0" ;
-				$settings['custom_pre'] = isset($_POST['custom_pre'])? $_POST['custom_pre'] : "" ;
-				$settings['custom_next'] = isset($_POST['custom_next'])? $_POST['custom_next'] : "" ;
-				$settings['custom_next_sub'] = isset($_POST['custom_next_sub'])? $_POST['custom_next_sub'] : "" ;
-				$settings['custom_pre_color'] = isset($_POST['custom_pre_color'])? $_POST['custom_pre_color'] : "" ;
-				$settings['custom_next_color'] = isset($_POST['custom_next_color'])? $_POST['custom_next_color'] : "" ;
-				$settings['custom_next_sub_color'] = isset($_POST['custom_next_sub_color'])? $_POST['custom_next_sub_color'] : "" ;
-				$settings['custom_pre_text_color'] = isset($_POST['custom_pre_text_color'])? $_POST['custom_pre_text_color'] : "" ;
-				$settings['custom_next_text_color'] = isset($_POST['custom_next_text_color'])? $_POST['custom_next_text_color'] : "" ;
-				$settings['custom_next_sub_text_color'] = isset($_POST['custom_next_sub_text_color'])? $_POST['custom_next_sub_text_color'] : "" ;
-				$settings['navi_img'] = isset($_POST['navi_img'])? "1" : "0" ;
-				$settings['pre_img_link'] = isset($_POST['pre_img_link'])? $_POST['pre_img_link'] : "" ;
-				$settings['next_img_link'] = isset($_POST['next_img_link'])? $_POST['next_img_link'] : "" ;
+				$settings['nav_within_cat'] = isset($_POST['nav_within_cat'])   ? "1" : "0" ;
+				$settings['is_active']      = isset($_POST['is_active'])        ? "1" : "0" ;
+				$settings['is_reversed']    = isset($_POST['is_reversed'])      ? "1" : "0" ;
+				$settings['position']       = $_POST['position']    ?? "bottom" ;
+				$settings['comingsoon']     = $_POST['comingsoon']  ?? "yes" ;
+				$settings['is_custom']      = $_POST['is_custom']   ?? "yes" ;
+				$settings['corner']         = $_POST['corner']      ?? "0" ;
+				$settings['shadow']         = $_POST['shadow']      ?? "yes" ;
+				$settings['related']        = $_POST['related']     ?? "yes" ;
+				$settings['style']          = $_POST['style_css']   ?? "" ;
+				$settings['is_button']      = isset($_POST['is_button'])        ? "1" : "0" ;
+				$settings['custom_pre']     = $_POST['custom_pre']  ??   "" ;
+				$settings['custom_next']    = $_POST['custom_next'] ??  "" ;
+				$settings['custom_next_sub']        = $_POST['custom_next_sub']         ??  "" ;
+				$settings['custom_pre_color']       = $_POST['custom_pre_color']        ??  "" ;
+				$settings['custom_next_color']      = $_POST['custom_next_color']       ??  "" ;
+				$settings['custom_next_sub_color']  = $_POST['custom_next_sub_color']   ??  "" ;
+				$settings['custom_pre_text_color']  = $_POST['custom_pre_text_color']   ??  "" ;
+				$settings['custom_next_text_color'] = $_POST['custom_next_text_color']  ??  "" ;
+				$settings['custom_next_sub_text_color'] = $_POST['custom_next_sub_text_color']  ??  "" ;
+				$settings['navi_img']       = isset($_POST['navi_img']) ? "1" : "0" ;
+				$settings['pre_img_link']   = $_POST['pre_img_link']    ??  "" ;
+				$settings['next_img_link']  = $_POST['next_img_link']   ??  "" ;
 				
 				update_option("wp_next_post_navi_options", serialize($settings));
 				echo '<div class="updated fade"><p>Setting Updated!</p></div>';
@@ -317,31 +317,31 @@ function nc_settings_link( $links ) {
 		{
 			$options = $this->get_wp_next_post_navi_options();
 			$options = array(
-				'is_active' => (isset($options) && is_array($options) && isset($options["is_active"]))?$options["is_active"]:'1',
-				'is_reversed' => (isset($options) && is_array($options) && isset($options["is_reversed"]))?$options["is_reversed"]:'0',
-				'position' => (isset($options) && is_array($options) && isset($options["position"]))?$options["position"]:'bottom',
-				'comingsoon' => (isset($options) && is_array($options) && isset($options["comingsoon"]))?$options["comingsoon"]:'yes',
-				'is_custom' => (isset($options) && is_array($options) && isset($options["is_custom"]))?$options["is_custom"]:'yes',
-				'corner' => (isset($options) && is_array($options) && isset($options["corner"]))?$options["is_custom"]:'corner',
-				'shadow' => (isset($options) && is_array($options) && isset($options["shadow"]))?$options["shadow"]:'yes',
-				'related' => (isset($options) && is_array($options) && isset($options["related"]))?$options["related"]:'yes',
-				'nav_within_cat' => (isset($options) && is_array($options) && isset($options["nav_within_cat"]))?$options["nav_within_cat"]:'0',
-				'style' => (isset($options) && is_array($options) && isset($options["style"]))?$options["style"]:'text-decoration: none;
-font:bold 16px sans-serif, arial;
-color: #666;',
-				'is_button' => (isset($options) && is_array($options) && isset($options["is_button"]))?$options["is_button"]:'1',
-				'custom_pre' => (isset($options) && is_array($options) && isset($options["custom_pre"]))?$options["custom_pre"]:'Previous Post',
-				'custom_next' => (isset($options) && is_array($options) && isset($options["custom_next"]))?$options["custom_next"]:'Next Post',
-				'custom_next_sub' => (isset($options) && is_array($options) && isset($options["custom_next_sub"]))?$options["custom_next_sub"]:'Next Post Coming Soon...',
-				'custom_pre_color' => (isset($options) && is_array($options) && isset($options["custom_pre_color"]))?$options["custom_pre_color"]:'#4CAF50',
-				'custom_next_color' => (isset($options) && is_array($options) && isset($options["custom_next_color"]))?$options["custom_next_color"]:'#008CBA',
-				'custom_next_sub_color' => (isset($options) && is_array($options) && isset($options["custom_next_sub_color"]))?$options["custom_next_sub_color"]:'#008CBA',
-				'custom_pre_text_color' => (isset($options) && is_array($options) && isset($options["custom_pre_text_color"]))?$options["custom_pre_text_color"]:'',
-				'custom_next_text_color' => (isset($options) && is_array($options) && isset($options["custom_next_text_color"]))?$options["custom_next_text_color"]:'',
-				'custom_next_sub_text_color' => (isset($options) && is_array($options) && isset($options["custom_next_sub_text_color"]))?$options["custom_next_sub_text_color"]:'',
-				'navi_img' => (isset($options) && is_array($options) && isset($options["navi_img"]))?$options["navi_img"]:'0',
-				'pre_img_link' => (isset($options) && is_array($options) && isset($options["pre_img_link"]))?$options["pre_img_link"]:'',
-				'next_img_link' => (isset($options) && is_array($options) && isset($options["next_img_link"]))?$options["next_img_link"]:'',
+				'is_active'         => $options["is_active"]        ?? '1',
+				'is_reversed'       => $options["is_reversed"]      ?? '0',
+				'position'          => $options["position"]         ?? 'bottom',
+				'comingsoon'        => $options["comingsoon"]       ?? 'yes',
+				'is_custom'         => $options["is_custom"]        ?? 'yes',
+				'corner'            => isset($options["corner"])    ?   $options["is_custom"] : 'corner',
+				'shadow'            => $options["shadow"]           ??  'yes',
+				'related'           => $options["related"]          ??  'yes',
+				'nav_within_cat'    => $options["nav_within_cat"]   ??  '0',
+				'style'             => $options["style"]            ??  'text-decoration: none;
+                                                                         font:bold 16px sans-serif, arial;
+                                                                         color: #666;',
+				'is_button'         => $options["is_button"]        ??  '1',
+				'custom_pre'        => $options["custom_pre"]       ??  'Previous Post',
+				'custom_next'       => $options["custom_next"]      ??  'Next Post',
+				'custom_next_sub'   => $options["custom_next_sub"]  ??  'Next Post Coming Soon...',
+				'custom_pre_color'  => $options["custom_pre_color"] ??  '#4CAF50',
+				'custom_next_color' => $options["custom_next_color"]??  '#008CBA',
+				'custom_next_sub_color'         => $options["custom_next_sub_color"]        ??  '#008CBA',
+				'custom_pre_text_color'         => $options["custom_pre_text_color"]        ??  '',
+				'custom_next_text_color'        => $options["custom_next_text_color"]       ??  '',
+				'custom_next_sub_text_color'    => $options["custom_next_sub_text_color"]   ??  '',
+				'navi_img'          => $options["navi_img"]         ??  '0',
+				'pre_img_link'      => $options["pre_img_link"]     ??  '',
+				'next_img_link'     => $options["next_img_link"]    ??  '',
 				
 			);
 			add_option("wp_next_post_navi_options", serialize($options));
